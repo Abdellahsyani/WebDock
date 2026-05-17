36408067cf322ff5adfa8be3044bb1e9e6763306
Abdellahsyani <syaniabdos@gmail.com>
2026-05-17 21:48:14 +0100

    just some practice

---
+++ practice/polyset/searchable_tree_bag.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "tree_bag.hpp"
+#include "searchable_bag.hpp"
+
+class saerchable_tree_bag : public tree_bag, public searchable_bag {
+  private:
+    bool search(node* node, const int value) const;
+  public:
+    searchable_tree_bag();
+    searchable_tree_bag(const searchable_tree_bag& other);
+    searchable_tree_bag& operator=(const searchable_tree_bag& other);
+    ~searchable_tree_bag();
+
+    bool has(int) const;
+}
