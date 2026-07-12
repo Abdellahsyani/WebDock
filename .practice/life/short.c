4fb0b2521ab0a371fdf6609ad35a82eafa946b87
Abdellahsyani <syaniabdos@gmail.com>
2026-07-12 15:39:13 +0100

    add some change

--- .practice/life/short.c
+++ .practice/life/short.c
@@ -3,15 +3,14 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-
 int main(int ac, char **av) {
-  if (ac != 4) {
+  if (ac != 4)
     return 1;
-  }
+
   int w = atoi(av[1]), h = atoi(av[2]), it = atoi(av[3]);
-  if (w <= 0 || h <= 0 || it < 0) {
+  if (w <= 0 || h <= 0 || it < 0)
     return 1;
-  }
+
   int board[h][w], tmp[h][w];
   for (int i = 0; i < h; i++) {
     for (int j = 0; j < w; j++) {
@@ -23,20 +22,21 @@ int main(int ac, char **av) {
   int x = 0, y = 0;
   bool pen = false;
   char c;
+
   while (read(0, &c, 1) == 1) {
-    if (c == 'w' && y > 0) {
+    if (c == 'w' && y > 0)
       y--;
-    } else if (c == 's' && y < h - 1) {
+    else if (c == 's' && y < h - 1)
       y++;
-    } else if (c == 'a' && x > 0) {
+    else if (c == 'a' && x > 0)
       x--;
-    } else if (c == 'd' && x < w - 1) {
+    else if (c == 'd' && x < w - 1)
       x++;
-    } else if (c == 'x')
+    else if (c == 'x')
       pen = !pen;
-    if (pen) {
+
+    if (pen)
       board[y][x] = 1;
-    }
   }
 
   for (int i = 0; i < it; i++) {
@@ -46,21 +46,19 @@ int main(int ac, char **av) {
         for (int c = 0; c < 9; c++) {
           int ny = y + (c / 3) - 1;
           int nx = x + (c % 3) - 1;
-          if (ny >= 0 && ny < h && nx >= 0 && nx < w) {
+          if (ny >= 0 && ny < h && nx >= 0 && nx < w)
             nb += board[ny][nx];
-          }
         }
         if (board[y][x])
           nb--;
-        if (board[y][x] && (nb == 2 || nb == 3)) {
+        if (board[y][x] && (nb == 2 || nb == 3))
           tmp[y][x] = 1;
-        } else if (!board[y][x] && (nb == 3)) {
+        else if (!board[y][x] && (nb == 3))
           tmp[y][x] = 1;
-        }
       }
     }
     for (int y = 0; y < h; y++) {
-      for (int x = 0; x < w; x++) {
+      for (int x = 0; x < w; x++){
         board[y][x] = tmp[y][x];
       }
     }
