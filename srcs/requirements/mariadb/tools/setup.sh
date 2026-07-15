baa6430c9e914c6fd0586b01499828da5a9496f5
Abdellahsyani <syaniabdos@gmail.com>
2026-07-15 15:12:41 +0100

    setup mariadb

---
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Start the MariaDB service in the background temporarily
+service mariadb start 
+
+# Wait a few seconds for the database to fully boot up
+sleep 5
+
+# Run the SQL commands to configure the database
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mariadb -e "FLUSH PRIVILEGES;"
+
+# Shut down the background service cleanly
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+# Start MariaDB in the foreground (PID 1) to keep the container alive
+echo "MariaDB is starting in the foreground..."
+exec mysqld_safe
