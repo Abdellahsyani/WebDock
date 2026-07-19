57c19295f55bb1d87acbae3c7ebba7af40ce7096
Abdellahsyani <syaniabdos@gmail.com>
2026-07-19 14:24:06 +0100

    .gitignore

---
+++ .practice/bsq/sim.h
@@ -0,0 +1,24 @@
+#ifndef SIM_H
+#define SIM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef struct s_point {
+  int x;
+  int y;
+} t_point;
+
+typedef struct s_game {
+  int width;
+  int height;
+  char empty;
+  char obstacle;
+  char draw;
+  char **cells;
+  t_point start;
+  int square_size;
+} t_game;
+
+#endif
